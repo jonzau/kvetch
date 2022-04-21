c03064606ddbe38320134da7f52d3b332f553017
Jonathan Z <izauritzb@gmail.com>
2022-04-21 11:38:48 -0400

    Add LOG_REQUESTS option. Improve requests' time tracking by switch from the 'http' package to 'request'. Improve stats logging.

--- server.js
+++ server.js
@@ -6,24 +6,36 @@ const INSTANCES = process.env.INSTANCES || 1;
 if (!URI) {
   throwInvalidURI();
 }
-const http = require('http');
+const request = require('request');
+
+const stats = {
+  totalTime: 0,
+  min: { instance: -1, time: 9999999999 },
+  max: { instance: -1, time: 0 },
+  totalErrors: 0,
+  errosMap: new Map()
+};
 
 function throwInvalidURI() {
   throw new Error('Invalid URI!');
 }
 
 function attack(attackCount) {
-  return new Promise((res, rej) => {
+  return new Promise((res, _rej) => {
     const start = Date.now();
-    return http.get(URI, (_resp) => {
-      const time = Date.now() - start;
-      console.log(`Request #${attackCount} (${URI}) ${time} ms`);
-      res({instance: attackCount, status: 'ok', time: time});
-    }).on("error", (err) => {
-      if (process.env.LOG_ERRORS === 'true') {
-        console.log(`Error (${URI}):`, err);
+    request.get({ url: URI, time: true }, (err, resp) => {
+      const time = resp?.elapsedTime | (Date.now() - start);
+      if (err) {
+        if (process.env.LOG_ERRORS === 'true') {
+          console.log(`Error (${URI}) ${time} ms:`, err);
+        }
+        res({instance: attackCount, status: 'error', time: time, error: err});
+      } else {
+        if (process.env.LOG_REQUESTS === 'true') {
+          console.log(`Request #${attackCount} (${URI}) ${time} ms`);
+        }
+        res({instance: attackCount, status: 'ok', time: time});
       }
-      res({instance: attackCount, status: 'error', error: err});
     });
   });
 }
@@ -36,38 +48,36 @@ for (let i = 0; i < INSTANCES; i++) {
 }
 
 Promise.all(instances).then(times => {
-  let totalTime = 0;
-  let min = {time: 9999999999};
-  let max = {time: 0};
-  let timedOut = 0;
-  let totalErrors = 0;
   times.forEach(attack => {
     switch (attack.status) {
       case 'ok':
-        if (attack.time > max.time) {
-          max.time = attack.time;
-          max.instance = attack.instance;
+        if (attack.time > stats.max.time) {
+          stats.max.time = attack.time;
+          stats.max.instance = attack.instance;
         }
-        if (attack.time < min.time) {
-          min.time = attack.time;
-          min.instance = attack.instance;
+        if (attack.time < stats.min.time) {
+          stats.min.time = attack.time;
+          stats.min.instance = attack.instance;
         }
-        totalTime += attack.time;
+        stats.totalTime += attack.time;
         break;
       case 'error':
-        if (attack.error.code === 'ETIMEDOUT') {
-          timedOut++;
-        }
-        totalErrors++;
+        stats.errosMap.set(attack.error.code,
+          { errno: attack.error.errno, count: (stats.errosMap.get(attack.error.code)?.count | 0) + 1 });
+          stats.totalErrors++;
         break;
       default:
     }
   });
-  const successfulCount = times.length - totalErrors;
-  const avg = totalTime / successfulCount;
-  console.log(`\nAverage time (${successfulCount} requests): ${avg.toFixed()} ms`);
-  console.log(`Max time (#${max.instance}): ${max.time} ms`);
-  console.log(`Min time (#${min.instance}): ${min.time} ms`);
-  console.log(`Timed out: ${timedOut}`);
-  console.log(`Total errors: ${totalErrors}`);
+  const successfulCount = times.length - stats.totalErrors;
+  const avg = stats.totalTime / successfulCount;
+  if (!isNaN(avg)) {
+    console.log(`\nAverage time (${successfulCount} requests): ${avg.toFixed()} ms`);
+    console.log(`Max time (#${stats.max.instance}): ${stats.max.time} ms`);
+    console.log(`Min time (#${stats.min.instance}): ${stats.min.time} ms`);
+  }
+  console.log(`Total errors: ${stats.totalErrors}`);
+  stats.errosMap.forEach((val, key) => {
+    console.log(`  ${key} [${val.errno}]: ${val.count}`);
+  });
 });
